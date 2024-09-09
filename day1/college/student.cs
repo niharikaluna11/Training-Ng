3074176c965ff972080e1aa9f1b989854f6e413c
Niharika-Garg <lunaa.ngg@gmail.com>
2024-09-09 23:52:11 +0530

    college project student & trial soln

---
+++ day1/college/student.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace college
+{
+    internal class student
+    {
+        
+
+
+
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTime DateOfBirth { get; set; }
+
+            public string Phone { get; set; }
+
+            public string Email { get; set; }
+
+
+
+            public void DisplayID()
+
+            {
+
+                Console.WriteLine("-------------------------------------");
+
+                Console.WriteLine($"Id : {Id}\tName: {Name}");
+
+                Console.WriteLine($"Date Of Birth : {DateOfBirth.ToString("dd-MMMM-yyyy")}");
+
+                Console.WriteLine($"Phone : {Phone}");
+
+                Console.WriteLine($"Email : {Email}");
+
+                Console.WriteLine("-------------------------------------");
+
+            }
+
+        }
+
+        internal class Program
+
+        {
+
+            static void Main(string[] args)
+
+            {
+
+                Console.WriteLine("Hello, World!");
+
+
+
+
+
+                student student = new student()
+
+                {
+
+                    Id = 101,
+
+                    Name = "Ramu",
+
+                    DateOfBirth = new DateTime(2001, 7, 12),
+
+                    Phone = "9876543210",
+
+                    Email = "ramu@gmail.com"
+
+                };
+
+
+
+
+
+                student.DisplayID();
+
+
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+
