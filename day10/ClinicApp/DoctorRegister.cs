dd6c552637f7b9a77c8f19c96fd1d93a9c9e1c82
Niharika Garg <lunaa.ngg@gmail.com>
2024-09-18 00:46:35 +0530

    Clinic Application main modules ready(1)

---
+++ day10/ClinicApp/DoctorRegister.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClinicApplication
+{
+    public class DoctorRegister
+    {
+        public void RegisterDoctor()
+        {
+           
+            Console.WriteLine("Registering as a Doctor");
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Specialization: ");
+            string specialization = Console.ReadLine();
+
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+
+            Console.Write("Enter User name: ");
+            string uname = Console.ReadLine();
+
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+             Doctor newDoctor = new Doctor()
+            {
+                Id = ClinicData.Doctors.Count + 1,
+                Name = name,
+                Specialization=specialization,
+                PhoneNumber = phoneNumber,
+                UserName = uname,
+                Email = email,
+                Password = password
+            };
+
+            ClinicData.Doctors.Add(newDoctor);
+            
+
+            Console.WriteLine("Doctor registered successfully!");
+            
+        }
+
+        public Doctor LoginDoctor()
+        {
+            
+
+            Console.WriteLine("Doctor Login");
+
+            Console.Write("Enter User Name: ");
+            string uname = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            // Find doctor in the ClinicData
+            Doctor doctor = ClinicData.Doctors
+                .FirstOrDefault(d => d.UserName == uname && d.Password == password);
+
+            if (doctor != null)
+            {
+                Console.WriteLine("Login successful!");
+                return doctor;
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials. Please try again.");
+                return null;
+            }
+           
+        }
+    }
+}
---
+++ day10/ClinicApp/PatientRegister.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicApplication
+{
+    public class PatientRegister
+    {
+        public void RegisterPatient()
+        {
+            
+            Console.WriteLine("Registering as a Patient");
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+
+            Console.Write("Enter User name: ");
+            string uname = Console.ReadLine();
+
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            Patient newPatient = new Patient
+            {
+                Id = ClinicData.Patients.Count + 1,  // Auto-generate a new ID
+                Name = name,
+                PhoneNumber = phoneNumber,
+                UserName = uname,
+                Email = email,
+                Password = password
+            };
+
+            // Add the new patient to the Patients list
+            ClinicData.Patients.Add(newPatient);
+
+            Console.WriteLine("Patient registered successfully!");
+            
+        }
+
+        public Patient LoginPatient()
+        {
+            
+            Console.WriteLine("Patient Login");
+
+            Console.Write("Enter User Name: ");
+            string uname = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            Patient patient =  ClinicData.Patients
+                .FirstOrDefault(d => d.UserName == uname && d.Password == password);
+
+            if (patient != null)
+            {
+                Console.WriteLine("Login successful!");
+                return patient;
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials. Please try again.");
+                return null;
+            }
+            
+        }
+    }
+
+}
