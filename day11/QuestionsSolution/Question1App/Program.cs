abb017c24e6a73abf9e98f41d864c6ca818cab52
Niharika Garg <lunaa.ngg@gmail.com>
2024-09-18 17:05:23 +0530

    Some Changes(!)

---
+++ day11/QuestionsSolution/Question1App/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Question1App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This is Question no. 1 !");
+
+            //https://www.hackerrank.com/challenges/plus-minus/problem?isFullScreen=true
+
+        }
+    }
+}
