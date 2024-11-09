c02d1bc4a0dd1df0c8b981773cdee80d435d01f4
Niharika Garg <lunaa.ngg@gmail.com>
2024-11-10 00:04:51 +0530

    Modified status date repo

--- Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Repositories/ComplaintStatusDateRepository.cs
+++ Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Repositories/ComplaintStatusDateRepository.cs
@@ -1,10 +1,11 @@
 ﻿using ComplaintTicketAPI.Context;
-using ComplaintTicketAPI.Models;
 using ComplaintTicketAPI.Interfaces;
+using ComplaintTicketAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ComplaintTicketAPI.Repositories
 {
+    //all done
     public class ComplaintStatusDateRepository : IRepository<int, ComplaintStatusDate>
     {
         private readonly ComplaintTicketContext _context;
@@ -16,42 +17,73 @@ namespace ComplaintTicketAPI.Repositories
 
         public async Task<ComplaintStatusDate> Add(ComplaintStatusDate entity)
         {
-            await _context.ComplaintStatusDates.AddAsync(entity);
-            await _context.SaveChangesAsync();
-            return entity;
+            try
+            {
+                await _context.ComplaintStatusDates.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch { throw new Exception("not able to add status date part"); }
+
         }
 
         public async Task<ComplaintStatusDate> Get(int id)
         {
-            return await _context.ComplaintStatusDates
-                .Include(csd => csd.Complaint)  // Include related entities if necessary
-                .Include(csd => csd.ComplaintStatus)
-                .FirstOrDefaultAsync(csd => csd.ComplaintId == id);
+
+            try
+            {
+                return await _context.ComplaintStatusDates
+               .Include(csd => csd.Complaint)  // Include related entities if necessary
+               .Include(csd => csd.ComplaintStatus)
+               .FirstOrDefaultAsync(csd => csd.ComplaintId == id);
+            }
+            catch { throw new Exception("not able to get status date part"); }
+
         }
 
         public async Task<IEnumerable<ComplaintStatusDate>> GetAll()
         {
-            return await _context.ComplaintStatusDates
+
+            try
+            {
+                return await _context.ComplaintStatusDates
                 .Include(csd => csd.Complaint)  // Include related entities if necessary
                 .Include(csd => csd.ComplaintStatus)
                 .ToListAsync();
+            }
+            catch { throw new Exception("not able to get status date part"); }
+
+
         }
 
         public async Task<ComplaintStatusDate> Update(ComplaintStatusDate entity)
         {
-            _context.ComplaintStatusDates.Update(entity);
-            await _context.SaveChangesAsync();
-            return entity;
+
+            try
+            {
+
+                _context.ComplaintStatusDates.Update(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch { throw new Exception("not able to get status date part"); }
+
         }
 
         public async Task Delete(int id)
         {
-            var complaintStatusDate = await _context.ComplaintStatusDates.FindAsync(id);
-            if (complaintStatusDate != null)
+            try
             {
-                _context.ComplaintStatusDates.Remove(complaintStatusDate);
-                await _context.SaveChangesAsync();
+
+                var complaintStatusDate = await _context.ComplaintStatusDates.FindAsync(id);
+                if (complaintStatusDate != null)
+                {
+                    _context.ComplaintStatusDates.Remove(complaintStatusDate);
+                    await _context.SaveChangesAsync();
+                }
             }
+            catch { throw new Exception("not able to get status date part"); }
+
         }
 
         public Task<ComplaintStatusDate> Update(ComplaintStatusDate entity, int key)
