339a09d8021ff8aac57a9064e93d2ceb4ffc183b
Niharika Garg <lunaa.ngg@gmail.com>
2024-11-10 00:04:44 +0530

    Modified complaint status profile

--- Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Mapper/ComplaintStatusProfile.cs
+++ Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Mapper/ComplaintStatusProfile.cs
@@ -1,30 +1,15 @@
-﻿using ComplaintTicketAPI.Models.DTO;
+﻿using AutoMapper;
+using ComplaintTicketAPI.Models.DTO;
 using ComplaintTicketAPI.Models;
-using AutoMapper;
 
-namespace ComplaintTicketAPI.Mapper
+public class ComplaintStatusProfile : Profile
 {
-    public class ComplaintStatusProfile : Profile
+    public ComplaintStatusProfile()
     {
-        public ComplaintStatusProfile()
-        {
-            // Map from ComplaintStatusDTO to ComplaintStatus
-            CreateMap<ComplaintStatusDTO, ComplaintStatus>()
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
-                .ForMember(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority))
-                .ForMember(dest => dest.CommentByUser, opt => opt.MapFrom(src => src.CommentByUser))
-                .ForMember(dest => dest.ComplaintStatusDates, opt => opt.MapFrom(src=>src.StatusDate)); // Ignoring ComplaintStatusDates as it might be handled separately
-
-            // Map from ComplaintStatusDTO to ComplaintStatusDate (for status date)
-            CreateMap<ComplaintStatusDTO, ComplaintStatusDate>()
-                .ForMember(dest => dest.StatusDate, opt => opt.MapFrom(src => src.StatusDate));
-        }
-
-        //CreateMap<ComplaintStatus, ComplaintStatusDTO>()
-        //    .ForMember(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority))
-        //    .ForMember(dest => dest.StatusDate, opt => opt.MapFrom(src => src.StatusDate))
-        //    // Add other necessary mappings
-        //    ;
-
+        CreateMap<ComplaintStatus, ComplaintStatusDTO>()
+            .ForMember(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+            .ForMember(dest => dest.StatusDate, opt => opt.MapFrom<StatusDateResolver>())  // Use the custom resolver here
+            .ForMember(dest => dest.CommentByUser, opt => opt.MapFrom(src => src.CommentByUser));
     }
 }
