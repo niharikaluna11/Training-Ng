4d3c1f60656a7bd318a0edd1d4d627fcc3eb7545
Niharika Garg <lunaa.ngg@gmail.com>
2024-11-09 23:59:26 +0530

    Resolver added

---
+++ Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Resolver/StatusDateResolver.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ComplaintTicketAPI.Models;
+using ComplaintTicketAPI.Models.DTO;
+using System;
+using System.Linq;
+
+public class StatusDateResolver : IValueResolver<ComplaintStatus, ComplaintStatusDTO, DateTime>
+{
+    public DateTime Resolve(ComplaintStatus source, ComplaintStatusDTO destination, DateTime destMember, ResolutionContext context)
+    {
+        return source.ComplaintStatusDates?
+                   .OrderByDescending(cs => cs.StatusDate)
+                   .FirstOrDefault()?.StatusDate ?? DateTime.MinValue;
+    }
+}
