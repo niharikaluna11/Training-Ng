e5f16208df143b9d452de11eebef1e593da2b309
Niharika Garg <lunaa.ngg@gmail.com>
2024-11-12 12:51:36 +0530

    Day45/vue js pratices

---
+++ Day45/trialapp/src/components/FirstComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+      <h2>{{ msg }}</h2>
+      <ul class="user-list">
+        <li v-for="item in dataList" :key="item.id" class="user-item">
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FirstComponent',
+    props: ['msg'],
+    data() {
+      return {
+        dataList: [
+          { id: 1, name: 'Johnny Naorem' },
+          { id: 2, name: 'Laishram Rohit' },
+          { id: 3, name: 'Niharika' },
+          { id: 4, name: 'Disha' }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .user-list {
+    list-style-type: none;
+    padding: 0;
+    margin-top: 20px;
+  }
+  
+  .user-item {
+    padding: 10px;
+    margin: 5px 0;
+    background-color: #f5f5f5;
+    border-radius: 8px;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+    font-size: 18px;
+    color: #333;
+    text-align: left;
+    max-width: 200px;
+    margin: auto;
+  }
+  </style>
+  
---
+++ Day45/trialapp/src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2>Productsss</h2>
+      <button @click="btnclick()" class="btn btn-success">clcick me</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProductList',
+    data(){
+        return {
+            btnclick:()=>
+        {
+            alert("clicked");
+        }
+
+        }
+    }
+    }
+  
+  </script>
+  
+  <style >
+
+  </style>
+  
