376006fb4a272fca7bd4ee6eaa3604b984179f33
Niharika Garg <lunaa.ngg@gmail.com>
2024-11-14 14:22:14 +0530

    Day 47 13.11 work vue api connect

---
+++ Day47/complaintticket-application/src/scripts/LoginService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+
+
+export function login(username, password) {
+  return axios.post('http://localhost:5062/api/User/LoginOFUser', {
+    "username": username,
+    "password": password
+  });
+}
