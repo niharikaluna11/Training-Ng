2e4d73b38dff302b966a6726282ec3665f9f16e5
Niharika Garg <lunaa.ngg@gmail.com>
2024-11-14 14:20:57 +0530

    Just Some config files of API changed

--- Day404142/ComplaintTicketSolution/ComplaintTicketAPI/obj/Debug/net6.0/ComplaintTicketAPI.AssemblyInfo.cs
+++ Day404142/ComplaintTicketSolution/ComplaintTicketAPI/obj/Debug/net6.0/ComplaintTicketAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComplaintTicketAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b0d3969ebf3dad7e6b6648760b8a703f2b053a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe292382d1ee107d7f4ce457b18cb4ea9b0b407")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComplaintTicketAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComplaintTicketAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
