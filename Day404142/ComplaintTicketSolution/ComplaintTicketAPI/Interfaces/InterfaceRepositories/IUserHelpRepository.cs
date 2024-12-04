f2279e5a563b5b69de8d7a14a2ad623ff138557f
Niharika Garg <lunaa.ngg@gmail.com>
2024-12-04 13:00:49 +0530

    user help desk repo added

---
+++ Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Interfaces/InterfaceRepositories/IUserHelpRepository.cs
@@ -0,0 +1,13 @@
+﻿using ComplaintTicketAPI.Models;
+
+namespace ComplaintTicketAPI.Interfaces.InterfaceRepositories
+{
+    public interface IUserHelpRepository
+    {
+        Task<IEnumerable<UserHelp>> GetAllQueriesAsync();
+        Task<UserHelp> GetQueryByEmailAsync(string email);
+        Task<UserHelp> GetQueryByEmailifAsync(string email);
+        Task AddQueryAsync(UserHelp userHelp);
+        Task UpdateQueryAsync(UserHelp userHelp);
+    }
+}
