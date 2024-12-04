e8a24d0be5d43247b28a638811e771caf065f97d
Niharika Garg <lunaa.ngg@gmail.com>
2024-12-04 13:00:03 +0530

    user help desk model added

---
+++ Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Models/UserHelp.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComplaintTicketAPI.Models
+{
+    public class UserHelp
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string email { get; set; }
+
+        public string query { get; set; }
+        public bool IsResponded { get; set; } = false;
+        public string? AdminResponse { get; set; }
+    }
+}
