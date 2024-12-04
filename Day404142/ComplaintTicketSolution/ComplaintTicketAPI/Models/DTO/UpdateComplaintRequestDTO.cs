2119c586f267c006859c97e417688367c9ea8519
Niharika Garg <lunaa.ngg@gmail.com>
2024-12-04 13:01:22 +0530

    modified dtos

--- Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Models/DTO/UpdateComplaintRequestDTO.cs
+++ Day404142/ComplaintTicketSolution/ComplaintTicketAPI/Models/DTO/UpdateComplaintRequestDTO.cs
@@ -3,8 +3,7 @@
     public class UpdateComplaintRequestDTO
     {
         public int ComplaintId { get; set; } // ID of the complaint being updated
-        public int OrganizationId { get; set; } // ID of the organization
-
+      
         public Status Status { get; set; } // New status for the complaint (Received, InProgress, Resolved)
         
         public string CommentByUser { get; set; } = string.Empty; // Comment provided by the user
